d8cd8903cc720bde17a02c08fd2a67947854220e
codeosh <derekcatigan@gmail.com>
2025-02-06 18:01:43 +0800

    commit

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,18 +1,16 @@
 {{-- resources\views\layouts\navbar.blade.php --}}
 <div class="navbar-header">
-    <div class="flex justify-between items-center">
-        <div class="logo-container">
-            <h5>POS</h5>
-        </div>
+    <div class="logo-container">
+        <h5>POS</h5>
+    </div>
 
-        <div class="profile-container mt-1">
-            <div class="avatar-container">
-                <img src="{{asset('images/pictures/Nitro-wallpaper.jpg')}}" alt="Profile Picture">
-            </div>
-            <div class="user-info">
-                <p>Derek Joshua T. Catigan</p>
-                <p>Admin</p>
-            </div>
+    <div class="profile-container">
+        <div class="avatar-container">
+            <img src="{{asset('images/pictures/Nitro-wallpaper.jpg')}}" alt="Profile Picture">
+        </div>
+        <div class="user-info">
+            <p>Derek Joshua T. Catigan</p>
+            <p>Admin</p>
         </div>
     </div>
 </div>
