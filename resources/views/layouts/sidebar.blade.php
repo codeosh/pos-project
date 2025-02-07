4f121d0ae34d6c1fd969a05e5bc8c11ce370a9ce
codeosh <derekcatigan@gmail.com>
2025-02-07 14:27:27 +0800

    commit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,22 @@
 {{-- resources\views\layouts\sidebar.blade.php --}}
 <div class="sidebar">
+    <div class="titleSide-container flex justify-center flex-col items-center text-nowrap">
+        <h5>BizmaTech - POS</h5>
+        <small>i World Solutions</small>
+    </div>
 
+    <div class="sidebar-container h-screen text-nowrap">
+        <ul class="side-items pt-5 ps-2 pe-2 pb-5 overflow-hidden">
+            <li class="item-list">
+                <a href="#" class="item-links">
+                    <i class="fa-solid fa-desktop"></i> Dashboard
+                </a>
+            </li>
+            <li class="item-list">
+                <a href="#" class="item-links">
+                    <i class="fa-solid fa-list"></i> Item Category
+                </a>
+            </li>
+        </ul>
+    </div>
 </div>
