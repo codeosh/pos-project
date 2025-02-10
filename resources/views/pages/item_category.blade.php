979e87e61069311f713d8a3b3b3fa25279e47cb4
codeosh <derekcatigan@gmail.com>
2025-02-10 13:58:50 +0800

    commit

--- resources/views/pages/item_category.blade.php
+++ resources/views/pages/item_category.blade.php
@@ -6,10 +6,10 @@
 @section('content')
 <div class="inline-flex items-center p-3 space-x-2">
     <div class="w-auto">
-        <x-bladewind::input placeholder="Code" size="small" style="width: 8rem;" />
+        <x-bladewind::input placeholder="Code" style="width: 10rem;" />
     </div>
     <div class="w-auto">
-        <x-bladewind::input placeholder="Description" size="small" style="width: 20rem;" />
+        <x-bladewind::input placeholder="Description" style="width: 30rem;" />
     </div>
 </div>
 @endsection
