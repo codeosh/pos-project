cdde56617be14e289700b3902d2bafc0da89707b
codeosh <derekcatigan@gmail.com>
2025-02-10 15:48:25 +0800

    commit

--- public/js/layouts/sidebar.js
+++ public/js/layouts/sidebar.js
@@ -1,5 +1,21 @@
 // public\js\layouts\sidebar.js
 $(document).ready(function () {
+    function checkScreenSize() {
+        if ($(window).width() <= 768) {
+            $(".sidebar").addClass("active");
+            $(".body-content").addClass("collapsed");
+        } else {
+            $(".sidebar").removeClass("active");
+            $(".body-content").removeClass("collapsed");
+        }
+    }
+
+    checkScreenSize();
+
+    $(window).resize(function () {
+        checkScreenSize();
+    });
+
     $("#menu-bars").click(function () {
         $(".sidebar").toggleClass("active");
         $(".body-content").toggleClass("collapsed");
--- resources/views/pages/item_category.blade.php
+++ resources/views/pages/item_category.blade.php
@@ -4,12 +4,36 @@
 @section('title', 'Item-Category - POS')
 
 @section('content')
-<div class="inline-flex items-center p-3 space-x-2">
-    <div class="w-auto">
-        <x-bladewind::input type="text" placeholder="Code" style="width: 10rem;" />
+<div class="flex flex-col items-center p-3 w-full">
+    <div class="flex flex-wrap items-center gap-2 pt-4 w-full justify-center">
+        <div class="w-auto">
+            <x-bladewind::input type="text" placeholder="Code" class="item-categoryInput code-input" />
+        </div>
+        <div class="w-auto">
+            <x-bladewind::input type="text" placeholder="Description" class="item-categoryInput" />
+        </div>
+        <div class="w-auto mb-4">
+            <x-bladewind::button>Add</x-bladewind::button>
+            <x-bladewind::button>Reset</x-bladewind::button>
+        </div>
     </div>
-    <div class="w-auto">
-        <x-bladewind::input type="text" placeholder="Description" style="width: 30rem;" />
+
+    {{-- Bladewind Table --}}
+    <div class="mt-6 w-full max-w-4xl overflow-x-auto">
+        <x-bladewind::table>
+            <x-slot name="header">
+                <th style="width: 12rem;">Code</th>
+                <th>Description</th>
+            </x-slot>
+            <tr>
+                <td>C1000</td>
+                <td>Sample Item</td>
+            </tr>
+            <tr>
+                <td>C1001</td>
+                <td>Sample Item</td>
+            </tr>
+        </x-bladewind::table>
     </div>
 </div>
 @endsection
