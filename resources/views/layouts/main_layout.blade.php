6193c57220b35a5b0ed67e45a5472241f8ae63c8
codeosh <derekcatigan@gmail.com>
2025-02-12 14:21:34 +0800

    commit

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -32,11 +32,6 @@
     </head>
 
     <body>
-        <!-- Loading Overlay -->
-        <div id="loadingOverlay" class="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
-            <div class="animate-spin rounded-full h-16 w-16 border-4 border-blue-500 border-t-transparent"></div>
-        </div>
-
         <div class="body-content bg-slate-100">
             @include('layouts.navbar')
 
