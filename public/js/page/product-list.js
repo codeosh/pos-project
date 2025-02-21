9dde05e126aff465d6d146e22e1b835ed468018c
angelica255255 <angelicanerves03@gmail.com>
2025-02-21 17:12:28 +0800

    commit

---
+++ public/js/page/product-list.js
@@ -0,0 +1,18 @@
+// public\js\page\product-list.js
+$(document).ready(function () {
+    const button = $("#dropdownButton");
+    const menu = $("#dropdownMenu");
+
+    button.click(function (event) {
+        menu.toggleClass("hidden");
+        menu.toggleClass("opacity-0 scale-95 opacity-100 scale-100");
+        event.stopPropagation(); // Prevents click from bubbling up
+    });
+
+    $(document).click(function (event) {
+        if (!menu.is(event.target) && !button.is(event.target) && menu.has(event.target).length === 0) {
+            menu.addClass("opacity-0 scale-95");
+            setTimeout(() => menu.addClass("hidden"), 200); // Hide after transition
+        }
+    });
+});
